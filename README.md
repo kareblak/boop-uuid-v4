7418e928f6112382e39807e7012ef6d76308b71c
Kåre Blakstad <kaare@griegconnect.com>
2024-06-28 09:57:00 +0200

    Script for generating UUID v4's with Boop

---
+++ README.md
@@ -0,0 +1,3 @@
+# Boop (MacOS tool) plugin for generating UUID v4
+
+Copy `uuid_v4.js` into your Boop plugins directory (configured through Boop settings)
---
+++ uuid_v4.js
@@ -0,0 +1,27 @@
+/**
+  {
+    "api":1,
+    "name":"Generate UUID V4",
+    "description":"Safely generate a UUID v4",
+    "author":"Kåre Blakstad",
+    "icon":"fingerprint",
+    "tags":"uuid,generate,v4"
+  }
+**/
+
+// https://stackoverflow.com/a/8809472
+function main(state) {
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    state.text = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
